047f0ba58bac85347c700399cc5b3b64a77af740
LiMaCor <julian.itsa8@gmail.com>
2025-08-06 20:40:00 +0200

    new: added second player to the game

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ FPS = 60
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
-YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_red.png"))
-RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 def draw():
     WINDOW.fill(WHITE)
 
     WINDOW.blit(YELLOW_SPACESHIP, (300, 100))
+    WINDOW.blit(RED_SPACESHIP, (700, 100))
 
     pygame.display.update()
 
