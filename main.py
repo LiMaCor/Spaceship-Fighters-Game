7cdb933e923680117924a89b4b3b7226637d0d97
LiMaCor <julian.itsa8@gmail.com>
2025-08-06 21:16:07 +0200

    new: added a way for players to fire bullets

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ BORDER = pygame.Rect(
 
 FPS = 60
 VEL = 5
+BULLET_VEL = 7
+MAX_BULLETS = 3
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
@@ -69,6 +71,9 @@ def main():
         SPACESHIP_HEIGHT
     )
     
+    red_bullets = []
+    yellow_bullets = []
+    
     clock = pygame.time.Clock()
     running = True
 
@@ -79,6 +84,25 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
                 break
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        yellow.x + yellow.width, 
+                        yellow.y + yellow.height / 2 - 2, 
+                        10, 
+                        5
+                    )
+                    
+                    yellow_bullets.append(bullet)
+                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        red.x, 
+                        red.y + red.height / 2 - 2, 
+                        10, 
+                        5
+                    )
+                    
+                    red_bullets.append(bullet)
         
         keys_pressed = pygame.key.get_pressed()
         yellow_handle_movement(keys_pressed, yellow)
