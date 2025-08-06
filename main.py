767ba726a783fe0002f1140afb66373604363c8b
Julián Martinez <julian.itsa8@gmail.com>
2025-08-06 13:56:46 +0200

    new: added function to draw on screen

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 
 WIDTH, HEIGHT = 1000, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Spaceship Fighters")
+
+WHITE = (255, 255, 255)
+
+def draw():
+    WINDOW.fill(WHITE)
+    pygame.display.update()
 
 def main():
     running = True
@@ -11,6 +18,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
                 break
+
+        draw()
 
     pygame.quit()
 
