4e818989d1a517f21bd6733a3f19c91148780e3b
Julián Martinez <julian.itsa8@gmail.com>
2025-08-06 14:07:03 +0200

    new: added players images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 1000, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -7,9 +8,19 @@ pygame.display.set_caption("Spaceship Fighters")
 WHITE = (255, 255, 255)
 
 FPS = 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_red.png"))
+RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
 
 def draw():
     WINDOW.fill(WHITE)
+
+    WINDOW.blit(YELLOW_SPACESHIP, (300, 100))
+
     pygame.display.update()
 
 def main():
@@ -18,7 +29,7 @@ def main():
 
     while running:
         clock.tick(FPS)
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
